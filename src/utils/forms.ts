a28c8b385b30f503ae0d7b5c5c3b88388b41b79a
Kenny Yu <kennychechuan.yu@gmail.com>
2026-09-18 13:29:16 +0200

    Forms now post to the coordinators' Google Sheet script

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -6,7 +6,7 @@
  * TMM inbox (code and setup notes: Work/TMM/website/forms). While it is
  * empty, each form falls back to its Formspree address.
  */
-export const FORMS_ENDPOINT = "";
+export const FORMS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxCZ99hnsIkiqrQjw2fQTpqd7PMd69MA56JJIBxrStb3FiiyJGC3Rge9p85qHc3kDzi/exec";
 
 export const formAction = (formspreeId: string) =>
   FORMS_ENDPOINT || `https://formspree.io/f/${formspreeId}`;
